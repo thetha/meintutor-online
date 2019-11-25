f58a4a23294e42a4fb87cc270ab5686a679a1c04
thetha <Frundel+1!>
2019-11-25 20:07:12 +0100

    Testing Stuff

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,7 +6,7 @@ export default () => (
   <nav role="navigation">
     <ul className={styles.navigation}>
       <li className={styles.navigationItem}>
-        <Link to="/">Home</Link>
+        <Link to="/">Homebase</Link>
       </li>
       <li className={styles.navigationItem}>
         <Link to="/blog/">Blog</Link>
